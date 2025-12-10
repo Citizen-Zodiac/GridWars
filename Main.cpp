6c5e5440aa97ded8b2be81b623b4f4a961bbce9f
EpicFailure <moham3dibr4him@gmail.com>
2025-12-10 16:07:04 +0200

    Pyramid TicTacToe

--- Main.cpp
+++ Main.cpp
@@ -6,6 +6,7 @@
 #include "games/obstacles_xo.h"
 #include "games/Misere.h"
 #include "games/tic_4x4.h"
+#include "games/pyramid_tictactoe.h"
 using namespace std;
 
 void show_menu() {
@@ -15,13 +16,15 @@ void show_menu() {
     cout << "3. SUS" << endl;
     cout << "4. Obstacles X-O" << endl;
     cout << "5. Misere Tic Tac Toe" << endl;
-    cout<<"6. 4x4 X-0 (4x4 Tic-Tac-Toe)"<<endl;
+    cout << "6. 4x4 X-0 (4x4 Tic-Tac-Toe)" << endl;
+    cout << "7. Pyramid Tic-Tac-Toe" << endl;  // NEW
     cout << "0. Exit" << endl;
     cout << "Choose: ";
 }
 
 int main() {
     int choice;
+    srand(time(0));  // For computer moves
 
     do {
         show_menu();
@@ -34,13 +37,11 @@ int main() {
             GameManager<int> game(board, players, ui);
             game.run();
 
-            // Cleanup
             delete board;
             for (int i = 0; i < 2; ++i) delete players[i];
             delete[] players;
             delete ui;
         }
-
         else if (choice == 2) {
             XO_5_Board* board = new XO_5_Board();
             XO_5_UI* ui = new XO_5_UI();
@@ -48,83 +49,84 @@ int main() {
             GameManager<char> game(board, players, ui);
             game.run();
 
-            // Cleanup
             delete board;
             for (int i = 0; i < 2; ++i) delete players[i];
             delete[] players;
             delete ui;
         }
-
-        else if (choice == 3)
-        {
+        else if (choice == 3) {
             SUS_Board* board = new SUS_Board();
             SUS_UI* ui = new SUS_UI();
             Player<char>** players = ui->setup_players();
             GameManager<char> game(board, players, ui);
             game.run();
 
-            // Cleanup
             delete board;
             for (int i = 0; i < 2; ++i) delete players[i];
             delete[] players;
             delete ui;
         }
-        else if (choice == 4)
-        {
+        else if (choice == 4) {
             Obstacles_XO_Board* board = new Obstacles_XO_Board();
             Obstacles_XO_UI* ui = new Obstacles_XO_UI();
             Player<char>** players = ui->setup_players();
             GameManager<char>game(board, players, ui);
             game.run();
 
-            // Cleanup
             delete board;
             for (int i = 0; i < 2; ++i) delete players[i];
             delete[] players;
             delete ui;
-
         }
-        else if (choice == 5)
-        {
+        else if (choice == 5) {
             MisereBoard* board = new MisereBoard();
             MisereUI* ui = new MisereUI();
-
             Player<char>** players = ui->setup_players();
-
             GameManager<char> game(board, players, ui);
             game.run();
 
-            // Cleanup
             delete board;
             for (int i = 0; i < 2; ++i) delete players[i];
             delete[] players;
             delete ui;
-
         }
-
         else if (choice == 6) {
             tic_4x4_Board* board = new tic_4x4_Board();
             tic_4x4_UI* ui = new tic_4x4_UI();
-            ui->set_board(board); // Connect UI to board
-
             Player<char>** players = ui->setup_players();
             GameManager<char> game(board, players, ui);
             game.run();
 
-            // Cleanup
             delete board;
             for (int i = 0; i < 2; ++i) delete players[i];
             delete[] players;
             delete ui;
         }
+        else if (choice == 7) {
+            // PYRAMID TIC-TAC-TOE
+            PyramidTicTacToeBoard* board = new PyramidTicTacToeBoard();
+            PyramidTicTacToeUI* ui = new PyramidTicTacToeUI();
+            Player<char>** players = ui->setup_players();
+            GameManager<char> game(board, players, ui);
+            game.run();
 
-        else if (choice != 1 || choice != 2 || choice != 3 || choice!=4|| choice != 0) {
-            cout << "Invalid choice. Please try again." << endl;
+            delete board;
+            for (int i = 0; i < 2; ++i) delete players[i];
+            delete[] players;
+            delete ui;
+        }
+        else if (choice == 0) {
+            cout << "Goodbye!" << endl;
+        }
+        else {
+            cout << "Invalid choice!" << endl;
         }
 
+        cout << "\nPress Enter to continue...";
+        cin.ignore();
+        cin.get();
 
     } while (choice != 0);
 
-    cout << "Goodbye!" << endl;
     return 0;
 }
